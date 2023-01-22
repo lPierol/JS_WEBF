1190af72262c007b90a46fea8b371da6c94ec56b
Piero Rodriguez <pierorodriiguez@gmail.com>
2023-01-22 18:37:01 -0300

    final

---
+++ js/datos.js
@@ -0,0 +1,65 @@
+var productos= [
+  {
+    "id": 1,
+     "nombre": "Gigante noble",
+     "precio": 6,
+     "destacado": true,
+     "imagen": "./img/royalgiant.png",
+  },
+  {
+    "id": 2,
+     "nombre": "Esbirros",
+     "precio": 3,
+     "destacado": true,
+     "imagen": "./img/minion.png"
+  },
+  {
+    "id": 3,
+     "nombre": "Mosquetera",
+     "precio": 4,
+     "destacado": true,
+     "imagen": "./img/musketeer.png"
+  },
+  {
+    "id": 4,
+     "nombre": "Mago",
+     "precio": 5,
+     "destacado": true,
+     "imagen": "./img/wizard.png"
+  },
+  {
+    "id": 5,
+     "nombre": "Principe",
+     "precio": 5,
+     "destacado": true,
+     "imagen": "./img/prince.png"
+  },
+  {
+    "id": 6,
+     "nombre": "Rayo",
+     "precio": 6,
+     "destacado": true,
+     "imagen": "./img/lightning.png"
+  },
+  {
+    "id": 7,
+     "nombre": "Minero",
+     "precio": 3,
+     "destacado": true,
+     "imagen": "./img/miner.png"
+  },
+  {
+    "id": 8,
+     "nombre": "Tronco",
+     "precio": 2,
+     "destacado": true,
+     "imagen": "./img/the_log.png"
+  },
+  {
+    "id": 9,
+     "nombre": "Super Golem de Hielo",
+     "precio": 999999,
+     "destacado": false,
+     "imagen": "./img/supericegolem.png"
+  },
+]
